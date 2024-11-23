c5c487ee2a0a4e62561f70f57415eb3a7a3a7052
CamilleKoestli <91135499+CamilleKoestli@users.noreply.github.com>
2024-11-23 16:56:38 +0100

    wip: start priority but doesn't work

--- code/prog2/src/locomotivebehavior.cpp
+++ code/prog2/src/locomotivebehavior.cpp
@@ -28,14 +28,14 @@ void LocomotiveBehavior::run() {
             if (directionAvant) {
                 // Logic for moving forward
                 attendre_contact(22);
-                sharedSection->access(loco);
+                sharedSection->access(loco, 1);
 
                 attendre_contact(12);
                 sharedSection->leave(loco);
             } else {
                 // Logic for moving in reverse direction
                 attendre_contact(11);
-                sharedSection->access(loco);
+                sharedSection->access(loco,1);
 
                 attendre_contact(20);
                 sharedSection->leave(loco);
@@ -54,7 +54,7 @@ void LocomotiveBehavior::run() {
             if (directionAvant) {
                 // Logic for moving forward
                 attendre_contact(25);
-                sharedSection->access(loco);
+                sharedSection->access(loco,2);
                 diriger_aiguillage(10, DEVIE, 0);
                 diriger_aiguillage(13, DEVIE, 0);
 
@@ -65,7 +65,7 @@ void LocomotiveBehavior::run() {
             } else {
                 // Logic for moving in reverse direction
                 attendre_contact(14);
-                sharedSection->access(loco);
+                sharedSection->access(loco, 2);
                 diriger_aiguillage(10, DEVIE, 0);
                 diriger_aiguillage(13, DEVIE, 0);
 
--- code/prog2/src/sharedsection.h
+++ code/prog2/src/sharedsection.h
@@ -1,82 +1,92 @@
-//    ___  _________    ___  ___  ___ ____ //
-//   / _ \/ ___/ __ \  |_  |/ _ \|_  / / / //
-//  / ___/ /__/ /_/ / / __// // / __/_  _/ //
-// /_/   \___/\____/ /____/\___/____//_/   //
-//
-
 #ifndef SHAREDSECTION_H
 #define SHAREDSECTION_H
 
+#include <queue>
 #include <QDebug>
-
 #include <pcosynchro/pcosemaphore.h>
 #include <pcosynchro/pcomutex.h>
-
-#include "locomotive.h"
-#include "ctrain_handler.h"
 #include "sharedsectioninterface.h"
+#include "locomotive.h"
 
-/**
- * @brief La classe SharedSection implémente l'interface SharedSectionInterface qui
- * propose les méthodes liées à la section partagée.
- */
-class SharedSection final : public SharedSectionInterface {
+class SharedSection : public SharedSectionInterface {
 public:
-    /**
-     * @brief SharedSection Constructeur de la classe qui représente la section partagée.
-     * Initialisez vos éventuels attributs ici, sémaphores etc.
-     */
-    SharedSection() {
-        // TODO
-        section_semaphore.release();
-        isUsed = false;
+    SharedSection() : priorityMode(PriorityMode::HIGH_PRIORITY), isUsed(false) {
+        section_semaphore.release(); // Initialisation du sémaphore
     }
 
-    /**
-     * @brief access Méthode à appeler pour accéder à la section partagée, doit arrêter la
-     * locomotive et mettre son thread en attente si la section est occupée par une autre locomotive.
-     * Si la locomotive et son thread ont été mis en attente,
-     * le thread doit être reveillé lorsque la section partagée est à nouveau libre et
-     * la locomotive redémarée. (méthode à appeler un contact avant la section partagée).
-     * @param loco La locomotive qui essaie accéder à la section partagée
-     */
-    void access(Locomotive &loco) override {
-        // TODO
+    void request(Locomotive& loco, int priority) override {
         section_mutex.lock();
-        if (isUsed) {
-            loco.arreter();
+        requests.emplace(priority, loco.numero());
+        section_mutex.unlock();
+        afficher_message(qPrintable(QString("Locomotive %1 demande l'accès avec priorité %2.")
+                                    .arg(loco.numero()).arg(priority)));
+    }
+
+    void access(Locomotive& loco, int priority) override {
+        section_mutex.lock();
+
+        while (isUsed || (priorityMode == PriorityMode::HIGH_PRIORITY && hasHigherPriority(priority))) {
+            loco.afficherMessage("En attente...");
             section_mutex.unlock();
+            section_semaphore.acquire(); // Attend que la section soit libérée
+            section_mutex.lock();
         }
-        section_semaphore.acquire();
-        loco.demarrer();
+
         isUsed = true;
         section_mutex.unlock();
-        // Exemple de message dans la console globale
-        afficher_message(qPrintable(QString("The engine no. %1 accesses the shared section.").arg(loco.numero())));
+        afficher_message(qPrintable(QString("Locomotive %1 accède à la section partagée.")
+                                    .arg(loco.numero())));
     }
 
-    /**
-     * @brief leave Méthode à appeler pour indiquer que la locomotive est sortie de la section
-     * partagée. (reveille les threads des locomotives potentiellement en attente).
-     * @param loco La locomotive qui quitte la section partagée
-     */
-    void leave(Locomotive &loco) override {
-        // TODO
+    void leave(Locomotive& loco) override {
         section_mutex.lock();
         isUsed = false;
-        section_semaphore.release();
+
+        if (!requests.empty()) {
+            section_semaphore.release();
+            requests.pop();
+        }
+
         section_mutex.unlock();
-        // Exemple de message dans la console globale
-        afficher_message(qPrintable(QString("The engine no. %1 leaves the shared section.").arg(loco.numero())));
+        afficher_message(qPrintable(QString("Locomotive %1 quitte la section partagée.")
+                                    .arg(loco.numero())));
+    }
+
+    void togglePriorityMode() override {
+        section_mutex.lock();
+        if (priorityMode == PriorityMode::HIGH_PRIORITY) {
+            priorityMode = PriorityMode::LOW_PRIORITY;
+            afficher_message("Mode de priorité bas activé.");
+        } else {
+            priorityMode = PriorityMode::HIGH_PRIORITY;
+            afficher_message("Mode de priorité haut activé.");
+        }
+        section_mutex.unlock();
     }
 
 private:
-    /* A vous d'ajouter ce qu'il vous faut */
+    /**
+     * @brief Vérifie si une locomotive avec une priorité plus élevée est en attente.
+     * @param currentPriority La priorité de la locomotive actuelle.
+     * @return true si une locomotive avec une priorité plus élevée attend.
+     */
+    bool hasHigherPriority(int currentPriority) {
+        std::priority_queue<std::pair<int, int>> tempQueue = requests;
+        while (!tempQueue.empty()) {
+            const auto& [priority, locoId] = tempQueue.top();
+            if (priority > currentPriority) {
+                return true;
+            }
+            tempQueue.pop();
+        }
+        return false;
+    }
 
-    bool isUsed = false;
-    PcoSemaphore section_semaphore;
-    PcoMutex section_mutex;
+    bool isUsed; ///< Indique si la section est utilisée
+    PriorityMode priorityMode; ///< Mode de gestion des priorités
+    PcoSemaphore section_semaphore; ///< Sémaphore pour gérer l'accès à la section
+    PcoMutex section_mutex; ///< Mutex pour protéger les ressources critiques
+    std::priority_queue<std::pair<int, int>> requests; ///< File des demandes d'accès (priorité, ID locomotive)
 };
-
 
 #endif // SHAREDSECTION_H
